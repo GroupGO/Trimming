285bd247e6ace00acaf44731a4d39b2bd667403f
GroupGO <henry.ehlers@wur.nl>
2015-11-25 10:17:47 +0100

    Update CutAdapt.py
    changing it back

--- CutAdapt.py
+++ CutAdapt.py
@@ -9,7 +9,6 @@ A class to run CutAdapt on a single input file at a time.
 
 In order to provide readable and understandable code, the right indentation margin has been
 increased from 79 to 99 characters, which remains in line with Python-Style-Recommendation (
-
 https://www.python.org/dev/peps/pep-0008/) .This allows for longer, more descriptive variable
 and function names, as well as more extensive doc-strings.
 """
